ed1f8535dd52c5e128b421b0813d94e718d5febc
lavi <npt1237@gmail.com>
2022-01-24 00:34:11 +0900

    express 기본 설정

--- server/server.js
+++ server/server.js
@@ -2,14 +2,52 @@ import express from 'express';
 import path from 'path';
 import iniparser from 'iniparser';
 import mysql from 'mysql';
+import cors from 'cors';
+import multer from 'multer';
+import bcrypt from 'bcrypt';
 
-var config = iniparser.parseSync('server/config.ini');
+const upload = multer({ dist: './upload'});
 
-console.log(config.db.id);
+const config = iniparser.parseSync('server/config.ini');
+
+const db = mysql.createConnection({
+    host : config.db.host,  
+    user : config.db.user,
+    password : config.db.pw,
+    database : config.db.db
+});
+
+db.connect();
+
+// let sql = '';
+// conn.query(sql, (err, rows, fields)=>{
+// });
 
 var app = express();
+app.use(cors());
 app.use(express.static(path.join(__dirname, '../build')))
+app.use('/profile', express.static('./upload'));
 
 app.get('/', (req, res)=>{ res.sendFile(__dirname, '../build/index.html') });
 
+app.get('/api/profile/:imgname', (req, res)=>{
+    res.json()
+});
+
+app.post('/api/customers', upload.single('image'), (req, res)=>{
+    let sql = 'INSERT INTO user (id, pw, profileImg) VALUES (null, ?, ?)';
+    let img = '/image/' + req.file.filename;
+    let id = 'asd';
+    let params = [img, id, "asd"];
+    conn.query(sql, params, (err, rows, fields)=>{
+        res.send(rows);
+    });
+})
+
 app.listen(3030, console.log("서버 실행중"));
+
+
+// bcrypt.genSalt(saltRounds, (err, salt)=>{
+//     bcrypt.hash(myPlaintextPassword, salt, (err, hash)=>{
+//     });
+// });
