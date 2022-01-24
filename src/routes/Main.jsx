77995a3b779d2437a195147f025d0624748ef3fa
lavi <npt1237@gmail.com>
2022-01-25 01:33:34 +0900

    부트스트랩, 부트스트랩 아이콘 설정

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,25 +1,76 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import postImg from '../res/img/postImg.png';
 import '../style/pageStyle/Main.scss';
 
 function App() {
-const [test, changeTest] = useState("붸");
+  const [postData, changepostData] = useState("");
 
-function getPosts() {
-  fetch("http://localhost:3030/api/test")
-    .then((response) => response.json())
-    .then((data) => { changeTest(data.sex) })
-}
+  function getPosts() {
+    fetch("http://localhost:3030/api/main")
+      .then((response) => response.json())
+      .then((data) => data.data)
+      .then((data) => changepostData(data))
+      .catch(error => console.error(error))
+  }
 
-getPosts()
+  useEffect(()=>{getPosts()}, []);
 
-return (
-  <main>
-    <div>
-      {test}
-    </div>
-    <button type='button' className="btn btn-primary">Btn</button>
-  </main>
-);
+  function Post(props) {
+    let postNum = props.num;
+    let value = postData[postNum];
+    let title = "loading";
+    let content = "loading";
+    let imgPath = "";
+    if (value != undefined) {
+      title = value.title;
+      content = value.content;
+      imgPath = "http://localhost:3030/postImg/" + value.imgPath + ".webp";
+    }
+    return(
+      <section>
+        <div className='content'>
+          <img src={(value == undefined) ? postImg : imgPath} />
+          <div>
+            <h1>{title}</h1>
+            <p>{content}</p>
+          </div>
+        </div>
+        <div className='likeBtns'>
+          <div className='like'>
+            <i class="bi bi-hand-thumbs-up"></i>
+            <span>0</span>
+          </div>
+          <div className='dislike'>
+            <i class="bi bi-hand-thumbs-down"></i>
+            <span>0</span>
+          </div>
+        </div>
+      </section>
+    )
+  }
+
+  function Posts() {
+    return(
+      <>
+        <Post num={0} />
+        <Post num={1} />
+        <Post num={2} />
+        <Post num={3} />
+        <Post num={4} />
+        <Post num={5} />
+        <Post num={6} />
+        <Post num={7} />
+        <Post num={8} />
+        <Post num={9} />
+      </>
+    )
+  }
+
+  return (
+    <main>
+      <Posts />
+    </main>
+  );
 }
 
 export default App;
