532b5d7b081eaf4d23a72803e0eea157dfe42cb5
lavi <npt1237@gmail.com>
2022-02-01 17:59:33 +0900

    post 페이지 fetch 통신 만들고 대충 화면애 띄움

---
+++ src/pages/Post.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import postImg from '../res/img/postImg.png';
+// import '../style/pageStyle/Post.scss';
+
+function Post() {
+    const { postNum } = useParams();
+    const [postData, changepostData] = useState("");
+
+    function getPost() {
+        fetch(`http://localhost:3030/api/post/${postNum}`)
+            .then((response) => response.json())
+            .then((data) => data.data)
+            .then((data) => changepostData(data[0]))
+            .catch(error => console.error(error))
+    }
+
+    useEffect(()=>{getPost()}, []);
+
+    let title = "loading";
+    let content = "";
+    let imgPath = "";
+    let like = "0";
+    let dislike = "0";
+    let uploadDate = "loading";
+    if (postData !== undefined) {
+        title = postData.title;
+        content = postData.content;
+        imgPath = "http://localhost:3030/postImg/" + postData.imgPath + ".webp";
+        like = postData.likeCount;
+        dislike = postData.dislikeCount;
+        uploadDate = postData.uploadDate;
+    }
+
+    return (
+        <main>
+            <div className='topContents'>
+                <img src={(postData === undefined) ? postImg : imgPath} alt='' />
+                <h1>{title}</h1>
+                <p>{uploadDate}</p>
+                <span>{like}</span>
+                <span>{dislike}</span>
+            </div>
+            <div className='content'>{content}</div>
+        </main>
+    );
+}
+
+export default Post;
