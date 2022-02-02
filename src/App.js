16c6cfb0be206674042ec4ebc2687ec4f45a7d3f
lavi <npt1237@gmail.com>
2022-02-03 01:39:26 +0900

    아 씨발

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 // import { Main, Search, Signin, Signup, Post, Test } from './pages';
 import Main from './pages/Main';
-import Search from './pages/Search';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Post from './pages/Post';
@@ -16,7 +15,6 @@ return (
     <Header />
     <Routes>
         <Route path="/" element={<Main />} exact />
-        <Route path="/search" element={<Search />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/post/:postNum" element={<Post />} />
