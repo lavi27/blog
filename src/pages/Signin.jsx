8456b43ad1be600c5aa8a7b40fc3500e84a9309b
lavi <npt1237@gmail.com>
2022-02-08 02:15:05 +0900

    feat : Signin 페이지 스타일 기초 추가하고 로그인 기능 제작
    Signin.module.scss에 코드를 기초적으로 추가했고, 로그인 기능을 완성했다.
    * Signin.module.scss
      *
    * Signin.jsx
      * useNavigate 훅 추가해서 로그인 성공시 메인 페이지로 이동
      * fetch 모듈에서 axios 모듈로 대체
      * 로그인 실패시 signinCurrect state 변경해서 안내 띄우기
      * css module 방식을 사용

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,22 +1,39 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 import style from '../style/pageStyle/Signin.module.scss';
 
 function Signin() {
     const [id, changeId] = useState("");
     const [pw, changePw] = useState("");
+    const [signinCurrect, changeSigninCurrect] = useState(false);
+    const navigate = useNavigate();
 
     function submit() {
-        fetch("http://localhost:3030/api/signin", {method: 'post', body: JSON.stringify({id: "id", pw: "pw"})})
-            .then((response) => response.json())
-            .then((data) => console.log(data))
+        axios.post("http://localhost:3030/api/signin", { id, pw }, {credentials: 'include', proxy: true,  withCredentials: true})
+            .then((response) => response.data)
+            .then((data) => {
+                if (data.success === false) {
+                    changeSigninCurrect(true);
+                } else {
+                    navigate("/");
+                };
+            })
             .catch(error => console.error(error))
     }
 
     return (
-        <main>
-            <label htmlFor='id'>아이디</label><input onChange={ (e)=>{changeId(e.target.value)} } type='text' id='id' name='id'></input>
-            <label htmlFor='pw'>비밀번호</label><input onChange={ (e)=>{changePw(e.target.value)} } type='text' id='pw' name='pw'></input>
-            <input type='submit' value='로그인' onClick={ submit }></input>
+        <main className={style.main}>
+            <div>
+                <label htmlFor='id'>아이디</label>
+                <input onChange={ (e)=>{changeId(e.target.value)} } type='text' id='id' name='id'></input>
+            </div>
+            <div>
+                <label htmlFor='pw'>비밀번호</label>
+                <input onChange={ (e)=>{changePw(e.target.value)} } type='text' id='pw' name='pw'></input>
+            </div>
+            <input className={style.submit} type='submit' value='로그인' onClick={ submit }></input>
+            {(signinCurrect === true) ? <p>아이디 또는 비밀번호가 일치하지 않습니다.</p> : null}
         </main>
     );
 }
