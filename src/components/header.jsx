059164bb7ee1d732a3d56b479b057f01e7f4d95b
lavi <npt1237@gmail.com>
2022-02-08 01:58:24 +0900

    style, feat : 로그인 여부 확인하고 일부 메뉴 띄우기
    axios 모듈을 통해 api 서버에서 로그인 여부 받아와서 메뉴 바꾸기
    * header.jsx
      * app 함수 return 부분 줄바꿈 수정
      * <SigninNav/> 엘리먼트 만들어서 로그인 여부에 따라 메뉴 바꾸기
      * getLogging 함수로 로그인 여부 받아오기

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import logo from '../res/img/logo.svg';
 
 function App() {
-return (
-<>
-    <header>
-        <div className='headerInner'>
-            <div>
-                <Link to="/">
-                    <img src={logo} alt='logo' />
-                    <h3>Blog</h3>
-                </Link>
-            </div>
-            <nav>
-                <Link to="/">home</Link>
-                <Link to="/signin">sign in</Link>
-                <Link to="/signup">sign up</Link>
-            </nav>
-        </div>
-    </header>
-</>
-);
+    const [logging, changeLogging] = useState("");
+
+    function getLogging() {
+        axios.get("http://localhost:3030/api/logging", {credentials: 'include', proxy: true,  withCredentials: true})
+            .then((response) => response.data)
+            .then((data) => {changeLogging(data.logging); console.log(data)})
+            .catch(error => console.error(error))
+    }
+
+    function signOut() {
+        axios.post("http://localhost:3030/api/signOut", {credentials: 'include', proxy: true,  withCredentials: true})
+            .then((response) => {console.log("as"); window.location.replace("/")})
+            .catch(error => console.error(error))
+    }
+
+    useEffect(()=>{getLogging()}, []);
+
+    function SigninNav() {
+        if (logging === false) {
+            return (
+                <>
+                    <Link to="/signin">sign in</Link>
+                    <Link to="/signup">sign up</Link>
+                </>
+            );
+        } else {
+            return(<a onClick={ signOut }>sign out</a>);
+        }
+    }
+
+    return (
+        <>
+            <header>
+                <div className='headerInner'>
+                    <div>
+                        <Link to="/">
+                            <img src={logo} alt='logo' />
+                            <h3>Blog</h3>
+                        </Link>
+                    </div>
+                    <nav>
+                        <Link to="/">home</Link>
+                        <SigninNav />
+                    </nav>
+                </div>
+            </header>
+        </>
+    );
 }
 
 export default App;
