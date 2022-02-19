ea97c07e22ed42fb4fce9088fdfbd8c8e927da1b
lavi <npt1237@gmail.com>
2022-02-19 21:29:29 +0900

    STYLE : header 스타일 style과 분리 및 header.jsx 로그인 메뉴와 통신 주소 변경
    * header.jsx
      * import style 추가
      * api 통신 주소 도메인으로 변경
      * 로그인시 nav에 write 버튼 보이기
    
    * header.scss
      * nav a 요소 커서 속성 pointer
    
    * style.scss
      * @import header 제거

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import logo from '../res/img/logo.svg';
+import style from '../style/header.scss';
 
 function App() {
     const [logging, changeLogging] = useState("");
 
     function getLogging() {
-        axios.get("http://localhost:3030/api/logging", {credentials: 'include', proxy: true,  withCredentials: true})
+        axios.get("http://lavi-blog.kro.kr:3030/api/logging", {credentials: 'include', proxy: true,  withCredentials: true})
             .then((response) => response.data)
-            .then((data) => {changeLogging(data.logging); console.log(data)})
+            .then((data) => changeLogging(data.logging))
             .catch(error => console.error(error))
     }
 
     function signOut() {
-        axios.post("http://localhost:3030/api/signOut", {credentials: 'include', proxy: true,  withCredentials: true})
-            .then((response) => {console.log("as"); window.location.replace("/")})
+        axios.get("http://lavi-blog.kro.kr:3030/api/signOut", {credentials: 'include', proxy: true,  withCredentials: true})
+            .then(() => window.location.replace("/"))
             .catch(error => console.error(error))
     }
 
@@ -30,7 +31,12 @@ function App() {
                 </>
             );
         } else {
-            return(<a onClick={ signOut }>sign out</a>);
+            return(
+                <>
+                    <Link to="/write">write</Link>
+                    <a onClick={ signOut }>sign out</a>
+                </>
+            );
         }
     }
 
