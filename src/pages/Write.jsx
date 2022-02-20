cfc3fdbc0b09450c1c8db2a381d1e5e05586595d
lavi <npt1237@gmail.com>
2022-02-20 22:54:30 +0900

    FEAT : 통신 로직만 추가, 글자부분 인풋으로 변경, 스타일링, 시간 추가
    * Write.jsx
        * axios를 통해 백엔드와 post 통신하는 로직 추가
        * useState 통해 인풋 값 저장 및 통신
        * title 부분과 content 부분 input과 textarea로 변경 및 변경때마다 state 이용
        * 버튼 스타일링 및 클릭시 axios통신 실행

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import postImg from '../res/img/postImg.png';
 import style from '../style/pageStyle/Write.module.scss';
 
 function Write() {
+    const [title, changeTitle] = useState("");
+    const [content, changeContent] = useState("");
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let date = today.getDate();
+
+    function writePost() {
+        axios.post("http://lavi-blog.kro.kr:3030/api/write", { title, content }, {credentials: 'include', proxy: true,  withCredentials: true})
+            .then((response) => response.data)
+            .then((data) => {
+                window.location.replace("/");
+            })
+            .catch(error => console.error(error))
+    }
+
     return (
         <main className={style.main}>
             <div className={style.topContents}>
                 <div className={style.info}>
-                    <h1>title</h1>
-                    <p>uploadDate</p>
+                    <input onChange={ (e)=>{changeTitle(e.target.value)} } className={style.title} type='text'></input>
+                    <p>{`${year}-${month}-${date}`}</p>
                 </div>
             </div>
             <img src={ postImg } alt='' />
             
-            <div className={style.content}>content</div>
+            <textarea onChange={ (e)=>{changeContent(e.target.value)} } className={style.content}></textarea>
 
-            <div>Post</div>
+            <button className={style.postBtn} onClick={writePost}>Post</button>
         </main>
     );
 }
