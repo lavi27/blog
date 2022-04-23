5487994764d54055d2ad5b3ef3299481b63827c8
lavi <npt1237@gmail.com>
2022-04-23 13:53:45 +0900

    FEAT : 글 반응 기능 완성

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import postImg from '../res/img/postImg.png';
 import style from '../style/pageStyle/Post.module.scss';
@@ -14,20 +15,73 @@ function Post() {
         uploadDate: "____-__-__"
     });
 
-    const [likeIsClick, changelikeClick] = useState(false);
-    const [dislikeIsClick, changedisdislikeClick] = useState(false);
+    const [likeIsClick, changelikeClick] = useState(0);
+    const [dislikeIsClick, changedisdislikeClick] = useState(0);
 
     function getPost() {
-        fetch(`http://lavi-blog.kro.kr:3030/api/post/${postNum}`)
-            .then((response) => response.json())
-            .then((data) => data.data)
+        axios.get(`http://lavi-blog.kro.kr:3030/api/post/${postNum}`, {credentials: 'include', proxy: true,  withCredentials: true})
+            .then((response) => response.data)
             .then((data) => {
-                changepostData(data[0]);
+                const value = data.data[0];
+                value.likeCount = 0;
+                value.dislikeCount = 0;
+
+                data.reaction.forEach(({reaction, userNum}) => {
+                    if(userNum === data.userNum) {
+                        if(reaction === "1") {
+                            changelikeClick(true);
+                            value.likeCount--;
+                        } else if(reaction === "2") {
+                            changedisdislikeClick(true);
+                            value.dislikeCount--;
+                        }
+                    }
+
+                    if(reaction === "1") {
+                        value.likeCount++;
+                    } else if(reaction === "2") {
+                        value.dislikeCount++;
+                    }
+                });
+
+                changepostData(value);
             })
             .catch(error => console.error(error))
     }
 
     useEffect(()=>{getPost()}, []);
+
+    function clickLike() {
+        axios.post("http://lavi-blog.kro.kr:3030/api/reaction/", {postNum, reaction: (likeIsClick) ? 0 : 1}, {credentials: 'include', proxy: true,  withCredentials: true})
+            .then((response) => response.data)
+            .then((data) => {
+                if (data.success) {
+                    changelikeClick(!likeIsClick);
+                    if (dislikeIsClick) {
+                        changedisdislikeClick(!dislikeIsClick);
+                    }
+                } else {
+                    console.log(data.err)
+                }
+            })
+            .catch(error => console.error(error))
+    }
+
+    function clickDislike() {
+        axios.post("http://lavi-blog.kro.kr:3030/api/reaction/", {postNum, reaction: (dislikeIsClick) ? 0 : 2}, {credentials: 'include', proxy: true,  withCredentials: true})
+            .then((response) => response.data)
+            .then((data) => {
+                if (data.success) {
+                    changedisdislikeClick(!dislikeIsClick);
+                    if (likeIsClick) {
+                        changelikeClick(!likeIsClick);
+                    }
+                } else {
+                    console.log(data.err)
+                }
+            })
+            .catch(error => console.error(error))
+    }
 
     function PostImg() {
         if(postData.imgPath === "") {
@@ -52,8 +106,14 @@ function Post() {
             <div className={style.content}>{postData.content}</div>
 
             <div className={style.like}>
-                <span className={style.likeBtn}>    <i className={`bi bi-hand-thumbs-up${   (likeIsClick===true) ? "-fill" : ""}`}      onClick={()=>changelikeClick(!likeIsClick)}></i>            {postData.likeCount}</span>
-                <span className={style.dislikeBtn}> <i className={`bi bi-hand-thumbs-down${ (dislikeIsClick===true) ? "-fill" : ""}`}   onClick={()=>changedisdislikeClick(!dislikeIsClick)}></i>   {postData.dislikeCount}</span>
+                <span className={style.likeBtn}>
+                    <i className={`bi bi-hand-thumbs-up${(likeIsClick === true) ? "-fill" : ""}`} onClick={ clickLike }></i>
+                    {postData.likeCount + likeIsClick}
+                </span>
+                <span className={style.dislikeBtn}>
+                    <i className={`bi bi-hand-thumbs-down${(dislikeIsClick === true) ? "-fill" : ""}`} onClick={ clickDislike }></i>
+                    {postData.dislikeCount + dislikeIsClick}
+                </span>
             </div>
         </main>
     );
