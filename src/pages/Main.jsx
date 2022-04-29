a62a2f6e13e174fb9d606d3fd74b0eeadf1ace79
lavi <npt1237@gmail.com>
2022-04-30 04:47:17 +0900

    a

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import postImg from '../res/img/postImg.png';
 import style from '../style/pageStyle/Main.module.scss';
 
@@ -6,9 +7,14 @@ function Main() {
   const [postData, changepostData] = useState("");
 
   function getPosts() {
-    fetch("http://lavi-blog.kro.kr:3030/api/main")
-      .then((response) => response.json())
-      .then((data) => changepostData(data.data))
+    // fetch("http://lavi-blog.kro.kr:3030/api/main")
+    //   .then((response) => response.json())
+    //   .then((data) => changepostData(data.data))
+    //   .catch(error => console.error(error))
+
+    axios.get(`http://lavi-blog.kro.kr:3030/api/main`, {credentials: 'include', proxy: true,  withCredentials: true})
+      .then((response) => response.data)
+      .then((data) => {changepostData(data.data)})
       .catch(error => console.error(error))
   }
 
@@ -17,7 +23,7 @@ function Main() {
   function PostImg(props) {
     let value = postData[props.num];
 
-    if (value == undefined) {
+    if (value === undefined) {
       return(<img src={postImg} alt='' />);
     } else if (value.imgPath == null) {
       return(<div style={{width: "120px", height: "80px"}}></div>);
@@ -32,6 +38,8 @@ function Main() {
     let title = "loading", uploadDate = "loading";
     let content = "", postNum = "";
     let like = "0", dislike = "0";
+    let myReaction;
+    console.log(value)
     if (value !== undefined) { //나중에 축약해서 수정
       title = value.title;
       content = value.content;
@@ -39,9 +47,10 @@ function Main() {
       dislike = value.dislikeCount;
       uploadDate = value.uploadDate;
       postNum = value.postNum;
+      myReaction = parseInt(value.myReaction);
     }
     return(
-      <a href={(value === undefined) ? "" : `/post/${value.postNum}`}>
+      <a href={(value === undefined) ? "" : `/post/${postNum}`}>
         <section>
           <div className={style.content}>
             <PostImg num={props.num} />
@@ -53,11 +62,11 @@ function Main() {
           <div className={style.rightUi}>
             <div className={style.likeBtns}>
               <div className={style.like}>
-                <i className="bi bi-hand-thumbs-up"></i>
+                <i className={`bi bi-hand-thumbs-up${(myReaction === 1) ? "-fill" : ""}`}></i>
                 <span>{like}</span>
               </div>
               <div className={style.dislike}>
-                <i className="bi bi-hand-thumbs-down"></i>
+              <i className={`bi bi-hand-thumbs-down${(myReaction === 2) ? "-fill" : ""}`}></i>
                 <span>{dislike}</span>
               </div>
             </div>
