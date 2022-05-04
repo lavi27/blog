886879b1aa5ccecfcaece1fffe12248cd889f7a5
lavi <npt1237@gmail.com>
2022-05-04 23:37:18 +0900

    FEAT : write 썸네일 기능 포기

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -7,8 +7,7 @@ function Write() {
     const [headerInfo, changeHeaderInfo] = useState("");
     const [title, changeTitle] = useState("");
     const [content, changeContent] = useState("");
-    const [img, ChangeImg] = useState("");
-    const [previewImg, ChangePreviewImg] = useState(postImg);
+    // const [img, ChangeImg] = useState("");
     const [postCurrect, changepostCurrect] = useState(0);
     let today = new Date();
 
@@ -21,15 +20,15 @@ function Write() {
 
     useEffect(()=>{getLogging()}, []);
 
-    function renderImg(file) {
-        ChangeImg(file);
+    // function renderImg(file) {
+    //     ChangeImg(file);
 
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = (e)=>{
-            ChangePreviewImg(reader.result);
-        };
-    }
+    //     const reader = new FileReader();
+    //     reader.readAsDataURL(file);
+    //     reader.onload = (e)=>{
+    //         ChangePreviewImg(reader.result);
+    //     };
+    // }
 
     function writePost() {
         if (title === "") {
@@ -37,14 +36,14 @@ function Write() {
         } else if (content === "") {
             changepostCurrect(2);
         } else {
-            const formData = new FormData();
-            if (img !== "") {
-                formData.append('file', img);
-            }
-            formData.append('title', title);
-            formData.append('content', content);
+            // const formData = new FormData();
+            // if (img !== "") {
+            //     formData.append('file', img);
+            // }
+            // formData.append('title', title);
+            // formData.append('content', content);
 
-            axios.post("http://lavi-blog.kro.kr:3030/api/write", formData, { credentials: 'include', proxy: true,  withCredentials: true})
+            axios.post("http://lavi-blog.kro.kr:3030/api/write", {title, content, userNum: headerInfo.userNum}, { credentials: 'include', proxy: true,  withCredentials: true})
                 .then((response) => response.data)
                 .then((data) => {
                     if (data.success) {
@@ -53,7 +52,10 @@ function Write() {
                         changepostCurrect(3);
                     }
                 })
-                .catch(error => console.error(error))
+                .catch(error => {
+                    console.error(error);
+                    changepostCurrect(3);
+                })
         }
     }
 
@@ -62,19 +64,20 @@ function Write() {
             <div className={style.topContents}>
                 <div className={style.info}>
                     <input onChange={ (e)=>{changeTitle(e.target.value)} } className={style.title} type='text'></input>
-                    <p>{`${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`}</p><p>{headerInfo.id}</p>
+                    <p>{`${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`}</p>
+                    <p>{headerInfo.id}</p>
                 </div>
             </div>
 
-            <label htmlFor='imgInput'><img src={previewImg} alt='' /></label>
-            <input className={style.imgInput} id='imgInput' type='file' onChange={ (e)=>{renderImg(e.target.files[0]);} }></input>
+            {/* <label htmlFor='imgInput'><img src={previewImg} alt='' /></label> */}
+            {/* <input className={style.imgInput} id='imgInput' type='file' onChange={ (e)=>{renderImg(e.target.files[0]);} }></input> */}
             
             <textarea onChange={ (e)=>{changeContent(e.target.value)} } className={style.content}></textarea>
 
             <button className={style.postBtn} onClick={writePost}>Post</button>
             {(postCurrect === 1) ? <p>제목을 써주세요.</p> : null}
             {(postCurrect === 2) ? <p>내용을 써주세요.</p> : null}
-            {(postCurrect === 3) ? <p>알 수 없는 버그가 발견했습니다.</p> : null}
+            {(postCurrect === 3) ? <p>알 수 없는 오류가 발생했습니다.</p> : null}
         </main>
     );
 }
